4ccc8e5992cc2fdff40ec845bc12f16187825a08
freddy990117 <freddy990117@gmail.com>
2024-11-05 06:26:20 +0800

    Update Show Picture

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-const Picture = () => {
+const Picture = ({ data, index }) => {
   return (
-    <div className="pictures">
-      <div className="picture">
-        <div className="imageContainer">
-            <img src="" alt="" />
-        </div>
+    <div className="picture">
+      <p>photographer is : {data.photographer}</p>
+      <div className="imageContainer">
+        <img src={data.src.large} alt={index} />
       </div>
+      <p>
+        Download Here :
+        <a target="_blank " href={data.src.large}>
+          Click Me
+        </a>
+      </p>
     </div>
   );
 };
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -6,20 +6,26 @@ import axios from "axios";
 const Homepage = () => {
   const APIKey = "lL1HhRkMbVFuuRw16a0djqE25Rp09n4Mp1ySItXcFVyiSSZMZ5SpOviJ";
   const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
-  const [showPicture, Picture] = useState();
-
+  const [data, setData] = useState(null);
   const searchURL = async () => {
     const result = await axios.get(initialURL, {
       headers: { Authorization: APIKey },
       // photographer alt src.large
     });
-    Picture(result.data.photos);
-    console.log(result.data);
+    // Here is API's Data
+    setData(result.data.photos);
+    console.log(data);
   };
   return (
     <div style={{ minHeight: "100vh" }}>
       <Search searchURL={searchURL} />
-      <Picture />
+      <div className="pictures">
+        {/* logical operator */}
+        {data &&
+          data.map((d, index) => {
+            return <Picture data={d} index={index} />;
+          })}
+      </div>
     </div>
   );
 };
