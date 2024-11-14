3cdd1b8ff3a3fe8d8dde88e21ee46f32b71d756a
Freddy <freddy990117@gmail.com>
2024-11-14 14:43:33 +0800

    成功顯示圖片，剩下顯示更多圖片

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,12 +4,13 @@ import Picture from "../components/Picture";
 import axios from "axios";
 
 const Homepage = () => {
-  const APIKey = "lL1HhRkMbVFuuRw16a0djqE25Rp09n4Mp1ySItXcFVyiSSZMZ5SpOviJ";
-  const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
-  let searchURL = `https://api.pexels.com/v1/search?query=${input}&page=1&per_page=15`;
   // States
   const [data, setData] = useState(null);
   const [input, setInput] = useState("");
+
+  const APIKey = "lL1HhRkMbVFuuRw16a0djqE25Rp09n4Mp1ySItXcFVyiSSZMZ5SpOviJ";
+  const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+  let searchURL = `https://api.pexels.com/v1/search?query=${input}&page=1&per_page=15`;
 
   const search = async (url) => {
     const result = await axios.get(url, {
@@ -21,7 +22,9 @@ const Homepage = () => {
   // Show Picture immediately
   useEffect(() => {
     search(initialURL);
-  }, []);
+  }, [input]);
+  // More Picture Button
+  const morePicture = () => {};
   return (
     <div style={{ minHeight: "100vh" }}>
       <Search
@@ -36,6 +39,9 @@ const Homepage = () => {
           data.map((d, index) => {
             return <Picture data={d} index={index} />;
           })}
+      </div>
+      <div className="morePicture">
+        <button onClick={morePicture()}>More</button>
       </div>
     </div>
   );
